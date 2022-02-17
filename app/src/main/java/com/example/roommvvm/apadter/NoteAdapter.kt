a8dd715fb191ab8f9fffe335c4913b139645dff3
gukasyan-arman <gukasian.a.a@gmail.com>
2022-02-17 16:43:08 +0300

    Adapter and Model implementation

--- app/src/main/java/com/example/roommvvm/apadter/NoteAdapter.kt
+++ app/src/main/java/com/example/roommvvm/apadter/NoteAdapter.kt
@@ -1,23 +1,45 @@
 package com.example.roommvvm.apadter
 
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.roommvvm.R
+import com.example.roommvvm.databinding.ItemLayoutBinding
+import com.example.roommvvm.model.NoteModel
+import kotlinx.android.synthetic.main.item_layout.view.*
 
 class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
-    class NoteViewHolder(view: View): RecyclerView.ViewHolder(view)
+    var listNote = emptyList<NoteModel>()
+
+    class NoteViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        private val binding = ItemLayoutBinding.bind(view)
+        fun bind(note: NoteModel) {
+            binding.apply {
+                itemTitle.text = note.title
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return NoteViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(listNote[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listNote.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setList(list: List<NoteModel>) {
+        listNote = list
+        notifyDataSetChanged()
     }
 
 }
