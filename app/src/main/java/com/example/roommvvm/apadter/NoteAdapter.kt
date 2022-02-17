a8bcae4bead5090973cde3de16ddbc4b756d8a87
gukasyan-arman <gukasian.a.a@gmail.com>
2022-02-17 16:16:00 +0300

    Initialize Adapter for RecyclerView!!!

--- app/src/main/java/com/example/roommvvm/apadter/NoteAdapter.kt
+++ app/src/main/java/com/example/roommvvm/apadter/NoteAdapter.kt
@@ -1,4 +1,23 @@
 package com.example.roommvvm.apadter
 
-class NoteAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    class NoteViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
